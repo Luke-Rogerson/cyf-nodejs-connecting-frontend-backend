f5a5d3b7c082c3b00ef931d6b71da0c7e0a56359
Luke Rogerson <luke.rogerson@stint.co>
2021-06-06 14:33:10 +0100

    Fetch and display users

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const API_URL = "http://localhost:8000";
 
 function App() {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch(`${API_URL}/users`);
+        const json = await res.json();
+        setUsers(json);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!users && <p>Loading...</p>}
+      {users &&
+        users.map((user) => {
+          return (
+            <div key={user.id}>
+              <p>{user.name}</p>
+              <p>{user.email}</p>
+            </div>
+          );
+        })}
     </div>
   );
 }
