839721b341689143b99aba1153e9e66a80289067
Luke Rogerson <luke.rogerson@stint.co>
2021-06-06 14:33:59 +0100

    Fix CORS issue

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,22 @@
 import express from "express";
 import { users } from "./mockData.js";
+import cors from "cors";
 
 const app = express();
-const port = 3000;
+const port = 8000;
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
 
 app.get("/users", (_req, res) => {
   res.send(users);
+});
+
+app.get("/", (_req, res) => {
+  res.send("Hello");
 });
 
 app.listen(port, () => {
